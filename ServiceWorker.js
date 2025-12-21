150da1428c4fdaa916b15ba62b73be62304ac1e3
elor504 <74613768+elor504@users.noreply.github.com>
2025-12-21 14:28:38 +0200

    test

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "CloverBite _ Bad GOAT-Tammuz Game Of UR-0.0.4f0";
+const contentToCache = [
+    "Build/Ur_WebXR.loader.js",
+    "Build/Ur_WebXR.framework.js.unityweb",
+    "Build/Ur_WebXR.data.unityweb",
+    "Build/Ur_WebXR.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
