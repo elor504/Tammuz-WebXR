38b4305abc8c99491dbcc5f36fdc1e01ce42c4d9
elor <74613768+elor504@users.noreply.github.com>
2025-12-22 13:38:20 +0200

    no button movement

--- Build/Tammuz-WebXR.loader.js
+++ Build/Tammuz-WebXR.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 50738319,
+    wasmFileSize: 50737103,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
